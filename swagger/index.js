d9453cc1e4c213f2e9d1732f48763e8a50a5855f
Junhyuk Park <junhyeg08@gmail.com>
2023-06-09 09:42:51 +0900

    [#2]Feature: 로그인 기능 및 미들웨어 구성

---
+++ swagger/index.js
@@ -0,0 +1,37 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express API with Swagger",
+      version: "1.0.0",
+      description: "Block Fighters Swagger",
+    },
+    servers: [
+      {
+        url: `http://localhost:8000`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "apiKey",
+          name: "Authorization",
+          scheme: "bearer",
+          in: "header",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./routes/user/login.js", "./routes/quest/register.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { specs };
